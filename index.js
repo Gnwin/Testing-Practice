f0fdb42afe46ec446fef43dfd0dab56c8e9987f9
Godwin Nwachukwu <nwgwin@gmail.com>
2022-04-07 12:30:58 +0100

    add tests for reversing a string

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { errorMonitor } = require("events");
+
 class Calculator {
   add(){}
   subtract(){}
@@ -7,8 +9,15 @@ class Calculator {
 const calc = new Calculator();
 
 module.exports = {
-  stringLength: (string) => string.length,
-  reverseString: function(string) {},
+  stringLength: (string) => {
+    if (string.length < 0) {
+      throw "There is nothing to count";
+    } else if (string.length > 10) {
+      throw "string is not within allowed range";
+    }
+    return string.length;
+  },
+  reverseString: (string) => string.split('').reverse().join(''),
   calc: calc,
-  capitalize: function(string) {}
+  capitalize: (string) => {}
 }
--- index.test.js
+++ index.test.js
@@ -1,13 +1,25 @@
 
 const {stringLength, reverseString, calc, capitalize} = require('./index');
 
+//stringLength
 test('counts string characters', () => {
   expect(stringLength('Godwin')).toBe(6);
 });
 
-// test('counts string characters', () => {
-//   expect(reverseString('Godwin')).toBe();
-// });
+test('data type of counted characters', () => {
+  expect(typeof stringLength('Godwin')).not.toBe('string');
+});
+
+test('counted characters', () => {
+  expect(stringLength('Godwin')).not.toBeNull();
+});
+
+// reverseString
+test('reverses string characters', () => {
+  const value = 'niwdoG';
+  console.log(reverseString('Godwin'));
+  expect(reverseString('Godwin')).toBe(value);
+});
 
 // test('counts string characters', () => {
 //   expect(calc.add(7, 2)).toBe(9);
